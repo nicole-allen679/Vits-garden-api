67f064d605ddbac07d051f9cbf85f2c5b3236e3c
nicole-allen679 <nicole.allen679@gmail.com>
2021-05-18 17:04:50 -0400

    routes working

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ require('./models/UserModel')
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-//const authRoutes = require('authRoutes')
 const bodyParser = require('body-parser')
+const authRoutes = require('./routes/authRoutes')
 const User = mongoose.model('User')
+const cors = require('cors')
+
+app.use(cors())
 app.use(bodyParser.json())
-//app.use(authRoutes)
+// app.use(authRoutes)
 const mongoUri =
-  'mongodb+srv://admin:passwordpassword@cluster0.hwo1l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+'mongodb+srv://admin:passwordpassword@cluster0.hwo1l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -18,23 +21,41 @@ mongoose.connection.on('connected', () => {
   console.log('Connected to Mongo')
 })
 mongoose.connection.on('error', (err) => {
-  console.log('Error connecting to Mongo')
+  console.log('Error connecting to mongo', err)
 })
-
 app.listen('5000', () => {
   console.log('App is listening on port 5000')
 })
-
 app.get('/', (req, res) => {
-  res.send("Welcome to Vit's Garden")
+  res.send('Welcome to Vits Garden')
+})
+app.get('/users', (req, res) => {
+  User.find().then((allUsers) => res.status(200).json(allUsers))
 })
 app.post('/signup', (req, res) => {
-  const { email, password } = req.body
-  const user = new User({ email, password })
+  const { email, password, firstName, lastName } = req.body
+  const user = new User({ email, password, firstName, lastName })
   user
     .save()
     .then(() => {
-      console.log('user was saved')
+      res.status(200).send('user was added')
+    })
+    .catch((err) => console.log(err))
+})
+app.post('/signin', (req, res) => {
+  const { email, password } = req.body
+  User.findOne({ email: email })
+    .then((userExists) => {
+      // check if email and password is in object
+      if (!email || !password) {
+        return res
+          .status(422)
+          .send({ error: 'Must Provide email and password ' })
+      }
+      // check if email exists in db
+      if (!userExists) {
+        return res.status(404).send({ error: 'User not found' })
+      }
     })
     .catch((err) => console.log(err))
 })
